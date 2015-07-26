edbc4abaaae11dd8691abcc6968ea7d4d45032a5
hbateman <hugobateman@gmail.com>
2015-07-26 12:03:01 +1200

    implemented some unit test

--- spec/javascripts/tests/test_test.js
+++ spec/javascripts/tests/test_test.js
@@ -1,7 +1,7 @@
 /**
  * Created by hugobateman on 25/07/15.
  */
-
+//
 describe('PasswordController', function() {
   beforeEach(module('application'));
 
@@ -16,6 +16,39 @@ describe('PasswordController', function() {
     it('sets the strength to "strong" if the password length is >8 chars', function() {
       var $scope = {};
       var controller = $controller('PasswordController', { $scope: $scope });
+      $scope.password = 'longerthaneightchars';
+      $scope.grade();
+      expect($scope.strength).toEqual('strong');
+    });
+  });
+});
+
+describe('LoginCtrl', function() {
+  beforeEach(module('application'));
+
+  var $controller;
+
+  beforeEach(inject(function($injector) {
+     // Set up the mock http service responses
+     $httpBackend = $injector.get('$httpBackend');
+     // backend definition common for all tests
+     authRequestHandler = $httpBackend.when('GET', '/auth.py')
+                            .respond({userId: 'userX'}, {'A-Token': 'xxx'});
+
+     // Get hold of a scope (i.e. the root scope)
+     $rootScope = $injector.get('$rootScope');
+     // The $controller service is used to create instances of controllers
+     var $controller = $injector.get('$controller');
+
+     createController = function() {
+       return $controller('MyController', {'$scope' : $rootScope });
+     };
+   }));
+
+  describe('$scope.grade', function() {
+    it('sets the strength to "strong" if the password length is >8 chars', function() {
+      var $scope = {};
+      var controller = $controller('LoginCtrl', { $scope: $scope });
       $scope.password = 'longerthaneightchars';
       $scope.grade();
       expect($scope.strength).toEqual('strong');
