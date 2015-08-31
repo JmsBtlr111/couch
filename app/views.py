63f25c3f7004c49aae27e8d3cf70e545e035b791
hbateman <hugobateman@gmail.com>
2015-08-31 12:23:58 +1200

    More log stuff

--- app/views.py
+++ app/views.py
@@ -24,7 +24,7 @@ def log():
         log_dict = request.form.copy().to_dict()
         # for key, value in log_dict.items():
         #     print (key, value)
-        print('console_logs: ' + str(log_dict['console_logs[0]']))
+        print('console_logs: ' + str(log_dict[request.console_logs[0]]))
         print('Remote Address: ' + str(request.remote_addr))
         return ""
 
