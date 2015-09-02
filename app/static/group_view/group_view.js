03994a984fcb67f7bafaa28edf1ec39baf66e12c
James Butler <jmsbtlr111@gmail.com>
2015-09-02 19:15:02 +1200

    Changed BUFFER size to 3500ms

--- app/static/group_view/group_view.js
+++ app/static/group_view/group_view.js
@@ -44,7 +44,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
     }).
     factory('RdioPlayerFactory', function ($window, $timeout, $rootScope) {
         var factory = {};
-        var TRACK_CHANGE_BUFFER = 4000;
+        var TRACK_CHANGE_BUFFER = 3500;
 
         factory.last_track_playing = null;
 
