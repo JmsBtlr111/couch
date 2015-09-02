a6464259c79f9bedc32a1bb5002cf7c87805d53c
hbateman <hugobateman@gmail.com>
2015-09-02 19:52:14 +1200

    Changed buffer to 0

--- app/static/group_view/group_view.js
+++ app/static/group_view/group_view.js
@@ -44,7 +44,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
     }).
     factory('RdioPlayerFactory', function ($window, $timeout, $rootScope) {
         var factory = {};
-        var TRACK_CHANGE_BUFFER = 3500;
+        var TRACK_CHANGE_BUFFER = 0;
 
         factory.last_track_playing = null;
 
