6035784670d43aa20b279dec446aa8aa98a5d267
hbateman <hugobateman@gmail.com>
2015-09-03 17:52:24 +1200

    Changed buffer to 2000ms

--- app/static/group_view/group_view.js
+++ app/static/group_view/group_view.js
@@ -44,7 +44,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
     }).
     factory('RdioPlayerFactory', function ($window, $timeout, $rootScope) {
         var factory = {};
-        var TRACK_CHANGE_BUFFER = 1500;
+        var TRACK_CHANGE_BUFFER = 2000;
 
         factory.last_track_playing = null;
 
