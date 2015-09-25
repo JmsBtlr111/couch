133423cae698723e8beb2129694c89f5a6206da5
James Butler <jmsbtlr111@gmail.com>
2015-09-25 13:31:47 +1200

    Added Clientside documentation

--- app/static/app.js
+++ app/static/app.js
@@ -1,12 +1,25 @@
 'use strict';
 
+/**
+ * The main app module declaration.
+ * Defines the top level dependencies required including routing and the other app views.
+ */
 angular
     .module('app', ['ui.router', 'app.home_view', 'app.group_view', 'app.login_view'])
+/**
+ * The main app configuration.
+ * Configures the app to redirect all unknown url paths to the root path.
+ */
     .config(['$urlRouterProvider',
         function ($urlRouterProvider) {
             $urlRouterProvider
                 .otherwise('/');
         }])
+/**
+ * The main app run function
+ * Performs login check each time the app's state (url path) is changed.
+ * If the user has not logged in, redirect them to the login page.
+ */
     .run(['$rootScope', '$state', '$window', '$http',
         function ($rootScope, $state, $window, $http) {
             $rootScope.$on('$stateChangeStart', function (event, to, toParams, from, fromParams) {
@@ -18,10 +31,11 @@ angular
                     return;
                 }
 
+                // This ensures that the currently listening users list is updated when the user leaves a group
                 if (from.name === 'group') {
                     $http.delete('https://couch.firebaseio.com/group/' + fromParams['id'] + '/listeners/' + $rootScope.current_user.firebase_id + '.json')
                         .error(function (data) {
-                            //console.log(data);
+                            console.log(data);
                         });
                 }
             });
--- app/static/group_view/group_view.js
+++ app/static/group_view/group_view.js
@@ -1,7 +1,16 @@
 'use strict';
 
-angular.module('app.group_view', ['ui.router', 'firebase']).
-    config(['$stateProvider',
+/**
+ * The group view module declaration.
+ * Declares the app router and firebase as dependencies.
+ */
+angular
+    .module('app.group_view', ['ui.router', 'firebase'])
+/**
+ * The group view configuration.
+ * Adds the group view's path to the routing logic. Requires login.
+ */
+    .config(['$stateProvider',
         function ($stateProvider) {
             $stateProvider.state('group', {
                 url: '/group/:id',
@@ -11,13 +20,20 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                     require_login: true
                 }
             });
-        }]).
-    factory('RdioSearchFactory', function ($window, $q) {
+        }])
+/**
+ * RdioSearchFactory definition.
+ * This factory returns an object with a method to search the Rdio music database.
+ */
+    .factory('RdioSearchFactory', function ($window, $q) {
         var factory = {};
 
+        // The search function returns a promise which resolves either to the search result or a rejection message.
+        // This allows the function to behave in an asynchronous manner.
         factory.search = function (search_text) {
             var deferred = $q.defer();
             var current_request;
+            // If search takes longer than 500ms, timeout.
             setTimeout(function () {
                 current_request = $window.R.request({
                     method: 'searchSuggestions',
@@ -41,13 +57,18 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
             return deferred.promise;
         };
         return factory;
-    }).
-    factory('RdioPlayerFactory', function ($window, $timeout, $rootScope) {
+    })
+/**
+ * The RdioPlayerFactory definition.
+ * This factory returns an object with methods to control the music playback.
+ */
+    .factory('RdioPlayerFactory', function ($window, $timeout, $rootScope) {
         var factory = {};
-        var TRACK_CHANGE_BUFFER = 0;
+        var INTER_TRACK_TIME_BUFFER = 500;
 
         factory.last_track_playing = null;
 
+        // This method plays the given track after the Inter-Track Time Buffer expires.
         factory.play = function(track) {
             var config = {'source': track.key};
             $timeout(function () {
@@ -61,11 +82,11 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                 $window.R.player.play(config);
                 $rootScope.tattletale.log('play_time: ' + (new Date).getTime());
                 $rootScope.finishedSong = false;
-            }, TRACK_CHANGE_BUFFER);
+            }, INTER_TRACK_TIME_BUFFER);
         };
 
-        // Play track from a position offset by a certain amount of time TODO: Test this works
-        factory.play_from_offset = function(track) {
+        // EXPERIMENTAL: This method plays the given track from an offset.
+        factory.playFromOffset = function(track) {
             console.log('playing ' + track.name);
             factory.last_track_playing = track;
             var time_since_track_moved_to_top_of_playlist = (new Date).getTime() - track.firebase_start_time;
@@ -74,6 +95,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
             $window.R.player.play(config);
         };
 
+        // EXPERIMENTAL: This method prefetches the track before the INTER_TRACK_TIME_BUFFER has expired to improve synchronicity.
         factory.playPausePlay = function(track) {
             factory.last_track_playing = track;
             var time_since_track_moved_to_top_of_playlist = (new Date).getTime() - track.firebase_start_time;
@@ -84,18 +106,36 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
             $timeout(function () {
                 $window.R.player.play();
                 console.log('Couch plays at: ' + (new Date).getTime())
-            }, 1000 - time_since_track_moved_to_top_of_playlist);
+            }, INTER_TRACK_TIME_BUFFER);
+        };
+
+        // Mutes the playback
+        factory.mute = function () {
+            $window.R.player.volume(0.0);
+        };
+
+        // Unmutes the playback
+        factory.maxVolume = function () {
+            $window.R.player.volume(1.0);
         };
 
         return factory;
-    }).
-    controller('GroupCtrl', ['$scope', '$stateParams', '$window', '$http', '$rootScope', '$firebaseArray','$firebaseObject', 'RdioSearchFactory', 'RdioPlayerFactory'/*, 'applicationLoggingService'*/,
-        function ($scope, $stateParams, $window, $http, $rootScope, $firebaseArray, $firebaseObject, RdioSearchFactory, RdioPlayerFactory/*, applicationLoggingService*/) {
+    })
+/**
+ * The group view's controller.
+ * Initializes client2server logging framework.
+ * Adds user to the group via Couch API.
+ * Updates currently listening user's list.
+ * Initializes playlist.
+ * Binds search + addToPlaylist functions to DOM elements.
+ * Handles distributed playback event logic.
+ */
+    .controller('GroupCtrl', ['$scope', '$stateParams', '$window', '$http', '$rootScope', '$firebaseArray','$firebaseObject', 'RdioSearchFactory', 'RdioPlayerFactory',
+        function ($scope, $stateParams, $window, $http, $rootScope, $firebaseArray, $firebaseObject, RdioSearchFactory, RdioPlayerFactory) {
             var firebase_group_url = 'https://couch.firebaseio.com/group/' + $stateParams.id;
             $rootScope.tattletale = new Tattletale('http://couch-music.herokuapp.com/log');
             $rootScope.finishedSong = false;
 
-            // TODO: Move this call to a state resolve function, if response code is 404 send user to home
             // Add current user to the current group in our db (expect 409 HTTP response code if user already in group)
             $http.post('/api/group/' + $stateParams.id, $rootScope.current_user)
                 .error(function (data, status) {
@@ -110,6 +150,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                 last_name: $rootScope.current_user.last_name
             };
 
+            // Initialize currently listening users list
             var listeners_ref = new Firebase(firebase_group_url + '/listeners');
             $scope.listeners = $firebaseArray(listeners_ref);
 
@@ -125,14 +166,16 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                     console.log(error);
                 });
 
+            // Initialize playlist
             var playlist_ref = new Firebase(firebase_group_url + '/playlist');
             $scope.playlist = $firebaseArray(playlist_ref);
 
             $scope.playlist.$loaded()
                 .then(function () {
                     $window.R.ready(function () {
+                        RdioPlayerFactory.maxVolume();
                         if ($scope.playlist.length) {
-                            RdioPlayerFactory.play_from_offset($scope.playlist[0]);
+                            RdioPlayerFactory.playFromOffset($scope.playlist[0]);
                         }
                     });
                 })
@@ -140,9 +183,12 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                     console.log(error);
                 });
 
+            // Playlist event handling logic.
             $scope.playlist.$watch(function (playlist_state) {
                 console.log(playlist_state);
+                // Handle a 'track added to playlist' event.
                 if (playlist_state.event.toString() == 'child_added'){
+                    // If the track added is the first in the playlist, start playback.
                     if (!playlist_state.prevChild) {
                         var first_track = $scope.playlist.$getRecord(playlist_state.key);
                         $rootScope.tattletale.log('first_in_playlist: True');
@@ -150,7 +196,10 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                     } else {
                         console.log('track added');
                     }
-                } else if (playlist_state.event.toString() === 'child_removed') {
+                }
+                // Handle a 'track removed from playlist' event, only the first track is ever removed.
+                else if (playlist_state.event.toString() === 'child_removed') {
+                    // If there are still more tracks on the playlist, play the next one.
                     if ($scope.playlist.length) {
                         var next_track_key = $scope.playlist.$keyAt(0);
                         var next_track = $scope.playlist.$getRecord(next_track_key);
@@ -162,11 +211,13 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                 }
             });
 
+            // Player event handling logic.
             $window.R.player.on('change:playingTrack', function (playing_track) {
                 console.log(playing_track);
+                // When a track finishes, remove the track from the playlist
                 if (!playing_track) {
                     var last_track_playing = RdioPlayerFactory.last_track_playing;
-                    // Check that the following variable exists for comparison
+                    // Check that the following variable exists before comparison
                     if (last_track_playing) {
                         $rootScope.finishedSong = true;
                         $scope.playlist.$remove(last_track_playing);
@@ -174,6 +225,7 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                 }
             });
 
+            // Initialize search logic.
             $scope.search_results = {};
 
             $scope.search = function (search_text) {
@@ -190,10 +242,14 @@ angular.module('app.group_view', ['ui.router', 'firebase']).
                 $scope.playlist.$add(track);
             };
 
-            $scope.sendLogs = function() {
-                $rootScope.tattletale.send()
-            };
+            // When a user leaves the group, mute playback and send client2server logs for that session.
+            $scope.$on('$destroy', function () {
+                RdioPlayerFactory.mute();
+                $rootScope.tattletale.send();
+            });
 
+            // In the case that the user closes the window:
+            // Remove the user from the currently listening users with a synchronous blocking API call.
             angular.element($window).bind('beforeunload', function () {
                 var request = new XMLHttpRequest();
                 request.open('DELETE',
